72591e9a5a88a06b95b6249cbdb0562cee109444
Setup Automação Mateus Quitério Marques <mateus.marques@setup.com.br>
2023-03-08 20:25:37 -0300

    Desenvolvido entidade da classe Animais

---
+++ Java/Aula1.java
@@ -0,0 +1,12 @@
+import entities.Animais;
+
+public class Aula1 
+{
+        public static void main(String[] args) throws Exception 
+    {
+        //System.out.println("Hello, Marques!");
+        Animais testeGustavo = new Animais(); // Instanciando a Classe()
+        String A = testeGustavo.ola(); // Variavel recebendo a classe testsGustavo
+        System.out.println(A); // Print 
+    }
+}
---
+++ Java/entities/Animais.java
@@ -0,0 +1,18 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package entities;
+
+/**
+ *
+ * @author lab4f
+ */
+public class Animais 
+{
+    
+    public static String caraAnimal1 = "Nascem, ";
+    public static String caraAnimal2 = "Comem, ";
+    public static String caraAnimal3 = "Andam. ";
+    
+}
