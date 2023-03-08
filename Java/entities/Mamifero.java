f5c4bb9ccdd45d598826c15dd215e5af7bfccc9b
Setup Automação Mateus Quitério Marques <mateus.marques@setup.com.br>
2023-03-08 20:32:23 -0300

    Desenvolvido entidade da classe Mamifero, com herança da classe Animais

--- Java/entities/Mamifero.java
+++ Java/entities/Mamifero.java
@@ -1,5 +1,16 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
 package entities;
 
-public class Mamifero {
+/**
+ *
+ * @author lab4f
+ */
+public class Mamifero extends Animais
+{
+    public static String caraMamiferos1= "\nTomam Leite e as femeas produzem o leite";
+    public static String caraMamiferos2= "\nPresença de pelos, diafragma e dentes adaptados aos alimentos";
     
 }
