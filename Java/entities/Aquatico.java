398859f2c2d52c0cab3b4d420a02eba2584b375d
Setup Automação Mateus Quitério Marques <mateus.marques@setup.com.br>
2023-03-08 20:29:08 -0300

    Desenvolvido entidade da classe Aquaticos, com herança da classe Animais

--- Java/entities/Aquatico.java
+++ Java/entities/Aquatico.java
@@ -1,5 +1,17 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
 package entities;
 
-public class Aquatico {
+/**
+ *
+ * @author lab4f
+ */
+public class Aquatico extends Animais // Herança
+{
+    
+    public static String caraAquatico1= "\nVivem na maior parte do tempo dentro da agua, seja ela de rios, lagos ou mares";
+    public static String caraAquatico2= "\nPodem ser animais (vertebrados e invertebrados)" + "ou outras formas de vida como, por exemplo, plantas, algas e planctons \n\n";
     
 }
