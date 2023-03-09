30f4d5999feeec0b53dd578c2910e0e50005ddcb
Setup Automação Mateus Quitério Marques <mateus.marques@setup.com.br>
2023-03-08 21:54:23 -0300

    Atualizado os arquivos

--- Java/Aula1.java
+++ Java/Aula1.java
@@ -1,20 +1,16 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-//package Aula1;
+// package Aula1;
 
+// Importando as entidades
 import entities.Aquatico;
 import entities.Mamifero;
 /**
  *
- * @author lab4f
+ * @author Mateus Marques
  */
 public class Aula1 
 {
     public Aula1()
-{    
-}
+{}
 
     /**
      * @param args the command line arguments
--- Java/entities/Mamifero.java
+++ Java/entities/Mamifero.java
@@ -1,12 +1,8 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package entities;
 
 /**
  *
- * @author lab4f
+ * @author Mateus Marques
  */
 public class Mamifero extends Animais
 {
