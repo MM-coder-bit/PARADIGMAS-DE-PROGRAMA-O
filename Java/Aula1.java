35d33e5313cedb3c29d47e7a84e8ff0eb8781f52
Mateus <marques18@outlook.com>
2023-03-22 21:22:09 -0300

    adicionado dois códigos com switch case em java, simulando um chatbot, sendo um deles com o do while

--- Java/Aula1.java
+++ Java/Aula1.java
@@ -22,7 +22,6 @@ public class Aula1
         System.out.println("Os animais Mamiferos: "+ Mamifero.caraAnimal1 + Mamifero.caraAnimal2 + Mamifero.caraMamiferos1 + Mamifero.caraMamiferos2);
         System.out.println("Os animais Aquaticos: "+ Aquatico.caraAnimal1 + Aquatico.caraAnimal2 + Aquatico.caraAquatico1 + Aquatico.caraAquatico2);
 
-
         // TODO code application logic here
     }
 }
---
+++ Java/chatBot.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+
+public class chatBot {
+
+   public static void main(String[] args) {
+      Scanner scanner = new Scanner(System.in);
+
+      System.out.println(
+            "Qual Materia ? (1 - Segunda, 2 - Terça, 3 - Quarta, 4 - Quinta, 5 - Sexta, 6 - Sábado, 7 - Domingo)\n");
+      int dia = scanner.nextInt();
+
+      switch (dia) {
+         case 1:
+            System.out.println("Você gosta de Segunda-feira!");
+            break;
+         case 2:
+            System.out.println("Você gosta de Terça-feira!");
+            break;
+         case 3:
+            System.out.println("Você gosta de Quarta-feira!");
+            break;
+         case 4:
+            System.out.println("Você gosta de Quinta-feira!");
+            break;
+         case 5:
+            System.out.println("Você gosta de Sexta-feira!");
+            break;
+         case 6:
+            System.out.println("Você gosta de Sábado!");
+            break;
+         case 7:
+            System.out.println("Você gosta de Domingo!");
+            break;
+         default:
+            System.out.println("Dia não reconhecido");
+            break;
+      }
+
+      scanner.close();
+   }
+}
